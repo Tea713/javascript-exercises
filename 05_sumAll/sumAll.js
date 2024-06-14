a99ef4ffbf01de5935850654debe65a381e0eadb
Tea713 <thanhnguyen_2025@depauw.edu>
2024-06-14 16:35:52 +0700

    Complete 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if(typeof start != "number" || typeof end != "number" || start < 0 || end < 0) {
+        return "ERROR";
+    }
+    result = 0;
+    if(start > end) {
+        temp = start;
+        start = end;
+        end = temp;
+    }
+    for(let i = start; i <= end; i++) {
+        result += i;
+    }
+    return result;
 };
 
 // Do not edit below this line
