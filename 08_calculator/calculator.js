3e7425f66789240cc84ef8b48eda07e03d70c143
Tea713 <thanhnguyen_2025@depauw.edu>
2024-06-14 17:00:51 +0700

    Complete 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, current) => { return total + current}, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, current) => { return total * current}, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(end) {
+	result = 1;
+  for(let i = 1; i <= end; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
