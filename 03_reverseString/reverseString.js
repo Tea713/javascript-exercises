64eb4aa33c866de046fb09ab84bc8b8163974812
Tea713 <thanhnguyen_2025@depauw.edu>
2024-06-14 15:43:23 +0700

    Complete 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(toReverse) {
+    result = "";
+    for(let i = toReverse.length - 1; i >= 0; i--) {
+        result += toReverse[i];
+    }
+    return result;
 };
 
 // Do not edit below this line
