8102f10d7c4ca17895f1aac5860f938c9158ad70
Tea713 <thanhnguyen_2025@depauw.edu>
2024-06-14 15:31:49 +0700

    Complete 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(toRepeat, times) {
+    if(times < 0) {
+        return "ERROR";
+    }
+    result = "";
+    for(let i = 0; i < times; i++) {
+        result += toRepeat;
+    }
+    return result;
 };
 
 // Do not edit below this line
