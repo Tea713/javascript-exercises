c9ca92b9c9d8cbbe6ab2c551a2d26e97e62cebc1
Tea713 <thanhnguyen_2025@depauw.edu>
2024-06-15 18:08:23 +0700

    Complete 12 and finished excercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) => {
+        let oldestAge = (("yearOfDeath" in oldest) ? oldest.yearOfDeath : new Date().getFullYear()) -  oldest.yearOfBirth;
+        let personAge = (("yearOfDeath" in person) ? person.yearOfDeath : new Date().getFullYear()) -  person.yearOfBirth;
+        return personAge > oldestAge ? person : oldest
+    }, people[0]);
 };
 
 // Do not edit below this line
