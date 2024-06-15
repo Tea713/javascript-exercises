641be289ab9a44ff8ffc12b137f8f1d39191a0b0
Tea713 <thanhnguyen_2025@depauw.edu>
2024-06-15 11:59:11 +0700

    Complete 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(x) {
+    if(typeof x === "string") {
+        x = parseInt(x);
+    }
+    if(x < 0) {
+        return "OOPS";
+    }
+    let a = 0;
+    let b = 1;
+    while(x-- > 0) {
+        b = a + b;
+        a = b - a;
+    }
+    return a;
 };
 
 // Do not edit below this line
