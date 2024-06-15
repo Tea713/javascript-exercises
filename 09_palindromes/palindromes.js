55aa6f5aab4168a34b34d99a22ceb2e22d19eaf2
Tea713 <thanhnguyen_2025@depauw.edu>
2024-06-15 11:45:58 +0700

    Complete 10

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
-
+const palindromes = function (pal) {
+    let palindrome = pal.toLowerCase();
+    let low = 0, high = pal.length - 1;
+    while(low < high) {
+        while(!(palindrome.charCodeAt(low) > 96 && palindrome.charCodeAt(low) < 123) && !(palindrome.charCodeAt(low) > 47 && palindrome.charCodeAt(low) < 58)) {
+            low++;
+        }
+        while(!(palindrome.charCodeAt(high) > 96 && palindrome.charCodeAt(high) < 123) && !(palindrome.charCodeAt(high) > 47 && palindrome.charCodeAt(high) < 58)) {
+            high--;
+        }
+        if(palindrome[low] !== palindrome[high]) {
+            return false;
+        }
+        low++;
+        high--;
+    }
+    return true;
 };
 
 // Do not edit below this line
